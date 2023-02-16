d2e90962a6bb8f8521e7cd01f0e9bfff3b78d1a8
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2023-02-16 18:27:46 +0500

    banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../App.css';
+
+export default function Banner() {
+  return (   
+      <div className="banner">
+        <span className='bannerText'>Turo is open for business in Australia! Tap to explore cars.</span>
+      </div>
+  )
+}
