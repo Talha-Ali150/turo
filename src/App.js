614e3380d88290b81296ed8723d891235be36cf2
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2023-02-16 22:11:02 +0500

    cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
+import ImgMediaCard from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Navbar />
-      <div className='mainImg'>
-      </div>
+      {/* <div className='mainImg'>
+      </div> */}
+      <ImgMediaCard />
       
       </div>
   );
---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../App.css";
+
+export default function Card() {
+  return (
+    <div className="carCard">
+      <div className="carImgContainer">
+      </div>
+      <p className="carCardText">Jeep</p>
+    </div>
+  );
+}
